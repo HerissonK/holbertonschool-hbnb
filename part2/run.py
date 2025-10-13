39c4d5af95b6d1745a9db3d6b693199de366f16d
HerissonK <11513@holbertonstudents.com>
2025-10-13 17:29:45 +0200

    implement post user

--- part2/run.py
+++ part2/run.py
@@ -1,6 +1,13 @@
-from app import create_app
+#from app import create_app
+from flask import Flask
+from flask_restx import Api
+from app.API.v1.users import api as users_api
 
-app = create_app()
+app = Flask(__name__)
+api = Api(app, version='1.0', title='HBNB API', description='Simple HBNB REST API')
+
+# Enregistrement du namespace
+api.add_namespace(users_api, path='/api/v1/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
