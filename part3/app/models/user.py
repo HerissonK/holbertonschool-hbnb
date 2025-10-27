8aefcb3c4465f74f1e627db998a77789346b2d1b
HerissonK <11513@holbertonstudents.com>
2025-10-27 16:03:32 +0100

    add password to user

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,5 +1,6 @@
 from app import db  # ton instance SQLAlchemy
 import uuid
+from flask_bcrypt import Bcrypt
 
 class User(db.Model):
     __tablename__ = "users"
@@ -8,8 +9,17 @@ class User(db.Model):
     first_name = db.Column(db.String(128), nullable=False)
     last_name = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
 
     def update(self, data):
         for key, value in data.items():
             if key != "id":
                 setattr(self, key, value)
+    
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
